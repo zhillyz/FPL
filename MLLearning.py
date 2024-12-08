587ab6303ac4674418a3ccf362200f4868ab0aff
Paul Hill <zhillyz@hotmail.com>
2024-12-08 21:05:17 +0000

    Added Machine Learning educational material

---
+++ MLLearning.py
@@ -0,0 +1,31 @@
+#Machine Learning worked example
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+#data inspection
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
+print(dataset.groupby('class').size())
+
+#data visualisation
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
